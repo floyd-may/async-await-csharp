a6b0a6bea1364c13407474ec654a875a59f60631
Floyd May <fmay@asemio.com>
2017-11-15 20:41:40 -0600

    look, it spins!

---
+++ AsyncAwait/AsyncAwait/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AsyncAwait
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+
+            var viewModel = new ViewModel();
+
+            var timer = new System.Timers.Timer
+            {
+                Interval = 100
+            };
+            
+            timer.Elapsed += (o, args) =>
+            {
+                Dispatcher.BeginInvoke(new Action(() => viewModel.RotationAngle = (int)(DateTime.Now.Millisecond * .36)));
+            };
+            timer.Start();
+
+            MainWindow.DataContext = viewModel;
+        }
+    }
+}
---
+++ AsyncAwait/AsyncAwait/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace AsyncAwait
+{
+    public sealed class ViewModel : INotifyPropertyChanged
+    {
+        private int _rotationAngle;
+
+        public int RotationAngle
+        {
+            get { return _rotationAngle; }
+            set
+            {
+                _rotationAngle = value;
+                OnPropertyChanged(nameof(RotationAngle));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
